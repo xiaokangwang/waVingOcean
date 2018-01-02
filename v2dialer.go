92957a79848bfb91db29b22af18c82cb4eb26b64
Shelikhoo <xiaokangwang@outlook.com>
2018-01-02 10:11:09 +0800

    Use new version of dialer

--- v2dialer.go
+++ v2dialer.go
@@ -6,13 +6,15 @@ import (
 	"net"
 	"strconv"
 
+	"v2ray.com/core/app"
 	"v2ray.com/core/app/dispatcher"
 	"v2ray.com/core/common/buf"
 	v2net "v2ray.com/core/common/net"
+	"v2ray.com/core/common/signal"
 )
 
 type V2Dialer struct {
-	ser *simpleServer
+	ser app.Space
 }
 
 func (vd *V2Dialer) Dial(network, address string, port uint16, ctx context.Context) (net.Conn, error) {
@@ -27,46 +29,42 @@ func (vd *V2Dialer) Dial(network, address string, port uint16, ctx context.Conte
 		log.Println(err)
 	}
 	v2dest := v2net.DestinationFromAddr(dest)
-	disp := dispatcher.FromSpace(vd.ser.space)
+	disp := dispatcher.FromSpace(vd.ser)
 	ray, err := disp.Dispatch(ctx, v2dest)
 	if err != nil {
 		panic(err)
 	}
 	//Copy data
 	conn1, conn2 := net.Pipe()
+
+	input := ray.InboundInput()
+	output := ray.InboundOutput()
+
+	requestDone := signal.ExecuteAsync(func() error {
+		defer input.Close()
+		v2reader := buf.NewReader(conn1)
+		if err := buf.Copy(v2reader, input); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	responseDone := signal.ExecuteAsync(func() error {
+		v2writer := buf.NewWriter(conn1)
+		if err := buf.Copy(output, v2writer); err != nil {
+			return err
+		}
+		return nil
+	})
+
 	go func() {
-		for {
-			var buffer [1500]byte
-			buf, err := ray.InboundOutput().ReadMultiBuffer()
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			n, err := buf.Read(buffer[:])
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			conn1.Write(buffer[:n])
+		if err := signal.ErrorOrFinish2(ctx, requestDone, responseDone); err != nil {
+			input.CloseError()
+			output.CloseError()
+			return
 		}
 	}()
-	go func() {
-		for {
-			mb := buf.NewMultiBufferCap(65536)
-			var buffer [1500]byte
-			n, err := conn1.Read(buffer[:])
-			if err != nil {
-				log.Println(err)
-				return
-			}
-			mb.Write(buffer[:n])
-			err = ray.InboundInput().WriteMultiBuffer(mb)
-			if err != nil {
-				log.Println(err)
-				return
-			}
-		}
-	}()
+
 	return conn2, nil
 }
 
