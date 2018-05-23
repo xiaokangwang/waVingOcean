43032d9ced2bf391ec45beb004bc6a7b8a6113e6
Shelikhoo <xiaokangwang@outlook.com>
2018-05-23 21:08:56 +0800

    Not tested and won't work

--- netstacklowerup.go
+++ netstacklowerup.go
@@ -28,6 +28,8 @@ import (
 type NetstackHolder struct {
 	nstack *netstack.Stack
 	dialer definition.SurrogateDialer
+	sgu    *Shuffler
+	inchan chan UDPPack
 }
 
 const (
@@ -97,7 +99,7 @@ func (nh *NetstackHolder) setupTCPHandler() error {
 
 func (nh *NetstackHolder) HandleTCPEndPoint(endpoint tcpip.Endpoint, wq *waiter.Queue) {
 	local, _ := endpoint.GetLocalAddress()
-	// TODO WARNING DANGEROUS ATTEMOT
+	// TODO WARNING DANGEROUS
 	addrs := local.Addr.String()
 	res, err := nh.dialer.Dial("tcp", addrs, local.Port, context.TODO())
 	if err != nil {
@@ -105,6 +107,50 @@ func (nh *NetstackHolder) HandleTCPEndPoint(endpoint tcpip.Endpoint, wq *waiter.
 		return
 	}
 	NewNSTunnel(endpoint, wq, res).Start()
+}
+
+func (nh *NetstackHolder) setupUDPHandler() error {
+	var wq waiter.Queue
+	ep, e := nh.nstack.NewEndpoint(udp.ProtocolNumber, 4, &wq)
+	if e != nil {
+		return errors.New(e.String())
+	}
+	defer ep.Close()
+	if err := ep.Bind(tcpip.FullAddress{NICId, "", netstackHookport}, nil); err != nil {
+		return errors.New(e.String())
+	}
+
+	// Wait for connections to appear.
+	waitEntry, notifyCh := waiter.NewChannelEntry(nil)
+	wq.EventRegister(&waitEntry, waiter.EventIn)
+	defer wq.EventUnregister(&waitEntry)
+	for {
+		var localAddr tcpip.FullAddress
+		v, err := ep.Read(&localAddr)
+		if err != nil {
+			if err == tcpip.ErrWouldBlock {
+				select {
+				case <-notifyCh:
+					continue
+				}
+			}
+			udp.UDPNatList.Delete(localAddr.Port)
+		}
+
+		endpointInterface, ok := udp.UDPNatList.Load(localAddr.Port)
+		if !ok {
+			udp.UDPNatList.Delete(localAddr.Port)
+			continue
+		}
+		endpoint := endpointInterface.(netstack.TransportEndpointID)
+		remoteHost := endpoint.LocalAddress.String()
+		remotePort := endpoint.LocalPort
+
+		localHost := endpoint.RemoteAddress.String()
+		localPort := endpoint.RemotePort
+		nh.sgu.Progress(v, remoteHost, remotePort, localHost, localPort)
+	}
+
 }
 
 func (nh *NetstackHolder) initializeStack(tunip string, ifce *water.Interface, mtu uint32) {
@@ -152,4 +198,7 @@ func (nh *NetstackHolder) initializeStack(tunip string, ifce *water.Interface, m
 			NIC:         NICId,
 		},
 	})
+	nh.inchan = make(chan UDPPack, 128)
+	UDPInjector(ifce, nh.inchan)
+	nh.sgu = NewShuffler(nh.dialer, nh.inchan)
 }
